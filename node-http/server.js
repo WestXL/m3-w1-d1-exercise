0626c77e1aa0aa29adff5e5b3ddbfe83e07c3ddc
WestXL <L.Xiao.191@westcliff.edu>
2022-06-28 20:27:03 -0700

    m3w1d1 ex2

--- node-http/server.js
+++ node-http/server.js
@@ -1,13 +1,15 @@
 var http = require('http');
 var hostname = 'localhost';
 var port = 3000;
+var path = require('path');
+var fs = require('fs');
 
 var server = http.createServer(function (req, res) {
     console.log(`Request for ${req.url} by method ${req.method}`);
     if (req.method === 'GET') {
         var fileUrl = req.url;
         if (fileUrl === '/') {
-            fileUrl = '/index.html';
+            fileUrl = '/aboutus.html';
         }
         var filePath = path.resolve('./public' + fileUrl);
         var fileExt = path.extname(filePath);
@@ -16,16 +18,12 @@ var server = http.createServer(function (req, res) {
                 if (err) {
                     res.statusCode = 404;
                     res.setHeader('Content-Type', 'text/html');
-                    res.end(`<html><body><h1>Error 404: ${fileUrl} not
-                              found</h1></body></html>`);
+                    res.end(`<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`);
                     return;
                 }
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'text/html');
                 fs.createReadStream(filePath).pipe(res);
-            });
-            server.listen(port, hostname, () => {
-                console.log(`Server running at http://${hostname}:${port}/`);
             });
         } else {
             res.statusCode = 404;
@@ -36,12 +34,8 @@ var server = http.createServer(function (req, res) {
         res.statusCode = 404;
         res.setHeader('Content-Type', 'text/html');
         res.end(`<html><body><h1>Error 404: ${fileUrl} is not an HTML file</h1></body></html>`);
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(`<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`);
     }
 });
-
-
-var path = require('path');
-var fs = require('fs');
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
